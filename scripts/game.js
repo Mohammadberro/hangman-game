47c1ffc830179f1125d492718c9314454409b5eb
Mohammadberro <ikerberro1@gmail.com>
2024-03-02 18:23:30 +0200

    adding active listener

--- scripts/game.js
+++ scripts/game.js
@@ -1,22 +1,34 @@
 const wordBank = ["pan", "arrive", "robust", "recognise", "example",
 "minute", "wooden", "spiky", "welcome", "appliance", "scientific"];
+
 let ranNum = Math.floor(Math.random() * wordBank.length);
 let ranWord = wordBank[ranNum];
 let ranWordLength = ranWord.length;
 let dashes = [];
-for (i = 0; i < ranWord.length; i++) {
-    dashes.push("_");
+let answerSection = document.getElementById("answer-section");
+const letters = document.querySelectorAll('.letter')
+
+function fillDashesList(ranWord){
+    for (i = 0; i < ranWord.length; i++) {
+        dashes.push("_");
+        }
     }
-function printDashes(dashes){
-    let answerSection = document.getElementById("answer-section");
-    for (i = 0; i < dashes.length;i++){
-        answerSection.innerHTML += '<div class="letter">' + dashes[i] + '</div>';
+
+fillDashesList(ranWord)
+
+console.log(dashes)
+
+function renderDashes(ranWord){
+    for (i = 0; i < ranWord.length;i++){
+        answerSection.innerHTML += `<div>${dashes[i]}</div>`;
     }
 
 }
-printDashes(dashes)
-// let letterDiv = document.querySelectorAll(".letter")
-// function clickedLetter{
-//     document.getElementsByClassName("")
-// }
-// letterClicked
+renderDashes(dashes)
+letters.forEach((element) => {
+    element.addEventListener('click', (e) => {
+      console.log('got clicked');
+      console.log(e);
+      console.log(e.target.innerText);
+    });
+  });
