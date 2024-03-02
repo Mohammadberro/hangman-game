83108ca617984fc2b9d002a068f307c194d50ba8
Mohammadberro <ikerberro1@gmail.com>
2024-03-02 23:20:26 +0200

    fixing error and finalizing

--- scripts/game.js
+++ scripts/game.js
@@ -1,15 +1,18 @@
 const wordBank = ["pan", "arrive", "robust", "recognise", "example",
-"minute", "wooden", "spiky", "welcome", "appliance", "scientific"];
+"minute", "wooden", "spiky", "welcome", "appliance", "scientific",
+ "gleaming", "shy", "crack", "partner", "exist", "train", "arrest",
+"trick", "bolt", "five", "guide", "nut", "parcel", "smiling"];
 
 let ranNum = Math.floor(Math.random() * wordBank.length);
 let ranWord = wordBank[ranNum];
 let dashes = [];
-let answerSection = document.getElementById("answer-section");
-const lettersQuery = document.querySelectorAll('.letter');
 let wrongGuesses = 0;
+const answerSection = document.getElementById("answer-section");
+const lettersQuery = document.querySelectorAll('.letter');
+
 
 function fillDashesList(ranWord){
-    for (i = 0; i < ranWord.length; i++) {
+    for (let i = 0; i < ranWord.length; i++) {
         dashes.push("_");
         }
     }
@@ -19,47 +22,76 @@ fillDashesList(ranWord)
 console.log(dashes);
 
 function updateDashes(letter, i) {
-            dashes[i] = letter;
+    dashes[i] = letter;
+    console.log(dashes);
 }
-
-console.log(dashes);
 
 function renderDashes(dashes){
     answerSection.innerHTML = ""
-    for (i = 0; i < dashes.length;i++){
+    for (let i = 0; i < dashes.length;i++){
         answerSection.innerHTML += `<div>${dashes[i]}&nbsp;</div>`;
     }
 
 }
+
 renderDashes(dashes);
+
+function renderHangman(wrongGuesses){
+    if (wrongGuesses === 1){
+        head();
+    }
+    else if (wrongGuesses === 2){
+        body();
+    }
+    else if (wrongGuesses === 3){
+        leftHand();
+    }
+    else if (wrongGuesses === 4){
+        rightHand();
+    }
+    else if (wrongGuesses === 5){
+        leftLeg();
+    }
+    else if (wrongGuesses === 6){
+        rightLeg();
+    }
+}
 
 function checkGuess(letter){
     let correctGuess = false;
     for (i = 0; i < ranWord.length; i++) {
         if (ranWord[i] == letter) {
             updateDashes(letter, i);
+            renderDashes(dashes);
             correctGuess = true;
         }
-    
     }
-}
+    if (dashes.join("") == ranWord){
+        console.log("You Won!");
+    }    
+    if (!correctGuess){
+        wrongGuesses += 1
+        renderHangman(wrongGuesses)
+        }
+    }
 function mouseClickedElements(lettersQuery){
     lettersQuery.forEach((element) => {
         element.addEventListener('click', (e) => {
-        console.log('got clicked');
-        console.log(e);
-        const pointerPressed = (e.target.innerText). toLowerCase();
-        console.log(pointerPressed)
-        checkGuess(pointerPressed);
+            console.log('got clicked');
+            console.log(e);
+            const pointerPressed = (e.target.innerText). toLowerCase();
+            console.log(pointerPressed)
+            checkGuess(pointerPressed);
         });
     });
 }
+
 mouseClicked = mouseClickedElements(lettersQuery);
 
 function keyPressed(){
     document.addEventListener('keydown', function(event) {
         const pressed = event.key;
-        console.log(pressed)
+        console.log(pressed);
         checkGuess(pressed);
     });
 }
